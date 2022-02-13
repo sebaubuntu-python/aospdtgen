22aaac1650dab8755661f38fef41b3f9755b8890
Albert I <kras@raphielgang.org>
2022-02-13 14:40:53 +0800

    aospdtgen: sections: Split Codec2 and OMX of Media
    Change-Id: I38e3634f04bd04215eeb5ea5181948b5883db0f7

--- aospdtgen/proprietary_files/sections/media.py
+++ aospdtgen/proprietary_files/sections/media.py
@@ -1,16 +1,19 @@
 from aospdtgen.proprietary_files.section import Section, register_section
 
-class MediaSection(Section):
-	name = "Media"
+class MediaCodec2Section(Section):
+	name = "Media (Codec2)"
 	interfaces = [
-		"android.hardware.media",
-		"android.hardware.media.bufferpool",
 		"android.hardware.media.c2",
-		"android.hardware.media.omx",
 		"vendor.qti.media.c2",
 	]
 	filenames = [
 		"c2_manifest_vendor.xml",
+	]
+
+class MediaOmxSection(Section):
+	name = "Media (OMX)"
+	interfaces = [
+		"android.hardware.media.omx",
 	]
 	patterns = [
 		"lib(64)?/libOmx.*\.so",
@@ -24,5 +27,6 @@ class MediaConfigsSection(Section):
 		"etc/media_profiles.*\.xml",
 	]
 
-register_section(MediaSection)
+register_section(MediaCodec2Section)
+register_section(MediaOmxSection)
 register_section(MediaConfigsSection)
