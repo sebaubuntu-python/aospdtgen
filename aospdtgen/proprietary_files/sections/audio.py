3486873a8bb29160239e5966aa3b59b1e178f921
Sebastiano Barezzi <barezzisebastiano@gmail.com>
2022-02-18 11:43:39 +0100

    aospdtgen: proprietary_files: sections: Add audio configs section
    Change-Id: Ia2c63120fe35c8ede66b1023e3bc150abf34d4f6

--- aospdtgen/proprietary_files/sections/audio.py
+++ aospdtgen/proprietary_files/sections/audio.py
@@ -20,5 +20,16 @@ class AudioFxModulesSection(Section):
 		"lib64/soundfx",
 	]
 
+class AudioConfigsSection(Section):
+	name = "Audio configs"
+	patterns = [
+		"etc/audio_configs.*\.xml",
+		"etc/audio_effects.*\.(conf|xml)",
+		"etc/audio_io_policy\.conf",
+		"etc/.*audio_policy.*\.xml",
+		"etc/audio_tuning_mixer\.txt",
+	]
+
 register_section(AudioSection)
 register_section(AudioFxModulesSection)
+register_section(AudioConfigsSection)
