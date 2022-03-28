5e9bd7d983d2e4dc7d690b6257e0c1c1512f651b
Sebastiano Barezzi <barezzisebastiano@gmail.com>
2022-03-28 21:00:32 +0200

    aospdtgen: lib: libvintf: Initial version
    Change-Id: I01e03637cbed6e82da2ef36a94ef725adfb1e117

---
+++ aospdtgen/lib/libvintf/__init__.py
@@ -0,0 +1,12 @@
+#
+# Copyright (C) 2022 The LineageOS Project
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+"""AOSP VINTF library."""
+
+from locale import LC_ALL, setlocale
+
+setlocale(LC_ALL, "C")
+
+INDENTATION = "    "
---
+++ aospdtgen/lib/libvintf/aidl.py
@@ -0,0 +1,65 @@
+#
+# Copyright (C) 2022 The LineageOS Project
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+from aospdtgen.lib.libvintf import INDENTATION
+from aospdtgen.lib.libvintf.common import Hal, cast_to_str_key
+from textwrap import indent
+from xml.etree.ElementTree import Element
+
+class AidlInterface:
+	def __init__(self, name: str, instance: str):
+		self.name = name
+		self.instance = instance
+
+	def __str__(self) -> str:
+		return f"{self.name}/{self.instance}"
+
+	def __eq__(self, __o: object) -> bool:
+		if isinstance(__o, AidlInterface):
+			return (self.name == __o.name
+			        and self.instance == __o.instance)
+		return False
+
+	def __hash__(self) -> int:
+		return hash((self.name, self.instance))
+
+	@classmethod
+	def from_fqname(cls, string: str) -> 'AidlInterface':
+		"""
+		Create a AIDL HAL from a FQName.
+
+		Example:
+		ILights/default
+		"""
+		name, instance = string.split("/", 1)
+
+		return cls(name, instance)
+
+class AidlHal(Hal):
+	def __init__(self, name: str, interfaces: set[AidlInterface]):
+		super().__init__(name)
+
+		self.interfaces = interfaces
+
+	def __str__(self) -> str:
+		string = f'<hal format="aidl">\n'
+		string += indent(f'<name>{self.name}</name>\n', INDENTATION)
+		for interface in sorted(self.interfaces, key=cast_to_str_key):
+			string += indent(f'<fqname>{interface}</fqname>\n', INDENTATION)
+		string += f'</hal>'
+
+		return string
+
+	@classmethod
+	def from_entry(cls, entry: Element) -> 'AidlHal':
+		"""Create a AIDL HAL from a VINTF entry."""
+		assert entry.get("format") == "aidl"
+
+		name = entry.findtext("name")
+		interfaces = set([AidlInterface.from_fqname(interface)
+		                  for interface in entry.findall("fqname")])
+
+		return cls(name, interfaces)
