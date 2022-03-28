cc733f075b9da1d82506efba9a38aea80fdb0f8d
Sebastiano Barezzi <barezzisebastiano@gmail.com>
2022-03-28 21:16:16 +0200

    aospdtgen: Use Path.cwd()
    Change-Id: I7980761e72d69b7a739d6ca74c5c35d771943b4f

--- aospdtgen/__init__.py
+++ aospdtgen/__init__.py
@@ -5,13 +5,12 @@
 #
 
 from aospdtgen.proprietary_files.section import register_sections
-import os
 from pathlib import Path
 
 __version__ = "0.1.0"
 
 module_path = Path(__file__).parent
 sections_path = module_path / "proprietary_files" / "sections"
-current_path = Path(os.getcwd())
+current_path = Path.cwd()
 
 register_sections(sections_path)
