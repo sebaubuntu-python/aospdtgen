b7fc53d6b8ab012231d6faa87dbf4752f4d6570c
Sebastiano Barezzi <seba@sebaubuntu.dev>
2024-02-11 06:18:09 +0100

    aospdtgen: Update kernel and boot partition flags
    Change-Id: I9389858ea872817e6bb8e6cdc8aab01cb7872a1c

--- aospdtgen/utils/boot_configuration.py
+++ aospdtgen/utils/boot_configuration.py
@@ -5,8 +5,8 @@
 #
 
 from pathlib import Path
-from sebaubuntu_libs.libaik import AIKManager
-from typing import Union
+from sebaubuntu_libs.libaik import AIKImageInfo, AIKManager
+from typing import Optional, Tuple, Union
 
 class BootConfiguration:
 	"""Class representing a device's boot configuration."""
@@ -19,55 +19,80 @@ class BootConfiguration:
 
 		self.boot = self._get_image_path("boot")
 		self.dtbo = self._get_image_path("dtbo")
+		self.init_boot = self._get_image_path("init_boot")
 		self.recovery = self._get_image_path("recovery")
 		self.vendor_boot = self._get_image_path("vendor_boot")
+		self.vendor_kernel_boot = self._get_image_path("vendor_kernel_boot")
 
 		assert self.boot, "No boot image found"
 
 		self.boot_aik_manager = AIKManager()
 		self.boot_image_info = self.boot_aik_manager.unpackimg(self.boot)
 
-		if self.recovery:
-			self.recovery_aik_manager = AIKManager()
-			self.recovery_image_info = self.recovery_aik_manager.unpackimg(self.recovery)
-		else:
-			self.recovery_aik_manager = None
-			self.recovery_image_info = None
+		self.init_boot_aik_manager, self.init_boot_image_info = self._extract_if_exists(
+			self.init_boot
+		)
 
-		if self.vendor_boot:
-			self.vendor_boot_aik_manager = AIKManager()
-			self.vendor_boot_image_info = self.vendor_boot_aik_manager.unpackimg(self.vendor_boot)
-		else:
-			self.vendor_boot_aik_manager = None
-			self.vendor_boot_image_info = None
+		self.recovery_aik_manager, self.recovery_image_info = self._extract_if_exists(
+			self.recovery
+		)
 
-		self.kernel = None
-		self.dt = None
-		self.dtb = None
+		self.vendor_boot_aik_manager, self.vendor_boot_image_info = self._extract_if_exists(
+			self.vendor_boot
+		)
 
-		self.cmdline = None
-		self.pagesize = None
+		self.vendor_kernel_boot_aik_manager, self.vendor_kernel_boot_image_info = self._extract_if_exists(
+			self.vendor_kernel_boot
+		)
+
+		self.kernel = self.boot_image_info.kernel
+		self.dt = self.boot_image_info.dt
+		self.dtb = self.boot_image_info.dtb
+
+		self.base_address = self.boot_image_info.base_address
+		self.cmdline = self.boot_image_info.cmdline
+		self.pagesize = self.boot_image_info.pagesize
 
 		if self.vendor_boot_image_info:
-			self.kernel = self.vendor_boot_image_info.kernel
-			self.dt = self.vendor_boot_image_info.dt
-			self.dtb = self.vendor_boot_image_info.dtb
-			self.dtbo = self.dtbo if self.dtbo else self.vendor_boot_image_info.dtbo
+			self.kernel = self.vendor_boot_image_info.kernel or self.kernel
+			self.dt = self.vendor_boot_image_info.dt or self.dt
+			self.dtb = self.vendor_boot_image_info.dtb or self.dtb
+			self.dtbo = self.vendor_boot_image_info.dtbo or self.dtbo
 
-			self.cmdline = self.vendor_boot_image_info.cmdline
-			self.pagesize = self.vendor_boot_image_info.pagesize
+			self.base_address = self.vendor_boot_image_info.base_address or self.base_address
+			self.cmdline = self.vendor_boot_image_info.cmdline or self.cmdline
+			self.pagesize = self.vendor_boot_image_info.pagesize or self.pagesize
 
-		self.kernel = self.kernel if self.kernel else self.boot_image_info.kernel
-		self.dt = self.dt if self.dt else self.boot_image_info.dt
-		self.dtb = self.dtb if self.dtb else self.boot_image_info.dtb
-		self.dtbo = self.dtbo if self.dtbo else self.boot_image_info.dtbo
+		if self.init_boot_image_info:
+			self.base_address = self.init_boot_image_info.base_address or self.base_address
+			self.cmdline = self.init_boot_image_info.cmdline or self.cmdline
+			self.pagesize = self.init_boot_image_info.pagesize or self.pagesize
 
-		self.cmdline = self.cmdline if self.cmdline else self.boot_image_info.cmdline
-		self.pagesize = self.pagesize if self.pagesize else self.boot_image_info.pagesize
+		if self.vendor_kernel_boot_image_info:
+			self.kernel = self.vendor_kernel_boot_image_info.kernel or self.kernel
+			self.dt = self.vendor_kernel_boot_image_info.dt or self.dt
+			self.dtb = self.vendor_kernel_boot_image_info.dtb or self.dtb
+			self.dtbo = self.vendor_kernel_boot_image_info.dtbo or self.dtbo
+
+			self.base_address = self.vendor_kernel_boot_image_info.base_address or self.base_address
+			self.cmdline = self.vendor_kernel_boot_image_info.cmdline or self.cmdline
+			self.pagesize = self.vendor_kernel_boot_image_info.pagesize or self.pagesize
 
 	def _get_image_path(self, partition: str) -> Union[Path, None]:
 		path = self.dump_path / f"{partition}.img"
 		return path if path.is_file() else None
+
+	@staticmethod
+	def _extract_if_exists(
+		image: Optional[Path]
+	) -> Tuple[Optional[AIKManager], Optional[AIKImageInfo]]:
+		if not image:
+			return None, None
+		
+		aik_manager = AIKManager()
+		image_info = aik_manager.unpackimg(image, ignore_ramdisk_errors=True)
+
+		return aik_manager, image_info
 
 	def copy_files_to_folder(self, folder: Path) -> None:
 		"""Copy all prebuilts to a folder."""
@@ -87,8 +112,14 @@ class BootConfiguration:
 		"""Cleanup all the temporary files. Do not use this object anymore after calling this."""
 		self.boot_aik_manager.cleanup()
 
+		if self.init_boot_aik_manager:
+			self.init_boot_aik_manager.cleanup()
+
 		if self.recovery_aik_manager:
 			self.recovery_aik_manager.cleanup()
 
 		if self.vendor_boot_aik_manager:
 			self.vendor_boot_aik_manager.cleanup()
+
+		if self.vendor_kernel_boot_aik_manager:
+			self.vendor_kernel_boot_aik_manager.cleanup()
