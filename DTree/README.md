502cd535ba006f99abd525753526231b2db2a30b
DestoryG <numbersans97@gmail.com>
2026-02-09 23:35:36 +0800

    chore: remove accidentally committed DTree output folder

--- DTree/README.md
+++
@@ -1,8 +0,0 @@
-# Android device tree for oplus ossi (ossi)
-
-```
-#
-# SPDX-FileCopyrightText: The LineageOS Project
-# SPDX-License-Identifier: Apache-2.0
-#
-```
--- DTree/rootdir/Android.mk
+++
@@ -1,8 +0,0 @@
-#
-# SPDX-FileCopyrightText: The LineageOS Project
-# SPDX-License-Identifier: Apache-2.0
-#
-
-LOCAL_PATH := $(call my-dir)
-
-# Recovery init configuration files
